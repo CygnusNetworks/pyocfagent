54f8b61af2ebe52bfb94f341edbe231384b242ac
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2014-05-21 12:16:57 +0200

    Add files for pypi distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ocfagent import __author__, __copyright__, __license__, __version__, __maintainer__, __email__, __status__ # pylint: disable=W0611
+from ocfagent import __author__, __copyright__, __license__, __version__, __maintainer__, __email__, __status__  # pylint: disable=W0611
 import setuptools
 
 setuptools.setup(name="ocfagent",
@@ -10,11 +10,15 @@ setuptools.setup(name="ocfagent",
 	author_email=__email__,
 	packages=["ocfagent"],
 	license=__license__,
+	platforms='POSIX',
+	url = 'https://github.com/CygnusNetworks/pyocfagent',
+	download_url = 'https://github.com/CygnusNetworks/pyocfagent/tarball/v0.10',
 	classifiers=[
 		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
 		"Programming Language :: Python",
 		"Development Status :: 4 - Beta",
 		"Operating System :: POSIX"
 	],
+	keywords=["pacemaker", "ocf", "agent", "python"],
 	install_requires=['lxml'],
 )
